23f7e7bd87f25c8d8346c7c61fe175ef78723121
firdavsxabibullayev800-commits <firdavsxabibullayev800@gmail.com>
2026-07-12 10:07:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
-from handlers import subscription, menu
+from database import init_db
+from handlers import subscription, menu, admin
+from middlewares.admin_forward import AdminForwardMiddleware
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
+    init_db()
 
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher(storage=MemoryStorage())
 
+    dp.message.outer_middleware(AdminForwardMiddleware())
+
     dp.include_router(subscription.router)
+    dp.include_router(admin.router)
     dp.include_router(menu.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
